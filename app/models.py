b7fa3b19483eeb77759e61c2d950e5b7a2169de8
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-22 09:22:23 -0700

    Add model for Posts table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -6,5 +7,20 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    # Define a one-to-many relationship.
+    # Will run a database query that returns all posts by the given user.
+    #   One:    user.posts
+    #   Many:   post.author (defined by backref)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __repr__(self):
-        return 'User {}'.format(self.username)
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
